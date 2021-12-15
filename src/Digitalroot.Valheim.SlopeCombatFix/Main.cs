5ba00cdd4168ba28673479fa409b7917eeb5c113
Nicholas Dunnaway <nick@dunnaway.org>
2021-12-14 23:21:05 -0800

    File import.

---
+++ src/Digitalroot.Valheim.SlopeCombatFix/Main.cs
@@ -0,0 +1,54 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System;
+
+namespace Digitalroot.Valheim.SlopeCombatFix
+{
+  [BepInPlugin(Guid, Name, Version)]
+  [BepInIncompatibility("hitbox.fix")]
+  public class Main : BaseUnityPlugin
+  {
+    public const string Version = "2.0.0";
+    public const string Name = "Digitalroot SlopeCombatFix";
+    public const string Guid = "digitalroot.mods.slopecombatfix";
+    public const string Namespace = "Digitalroot.Valheim" + nameof(SlopeCombatFix);
+    public static Main Instance;
+    private Harmony _harmony;
+
+    public ConfigEntry<float> Offset;
+    public ConfigEntry<float> Height;
+
+
+    public Main()
+    {
+      Instance = this;
+      Offset = Config.Bind<float>("Modify offset", "offset", 0f, "Offset");
+      Height = Config.Bind<float>("Modify height (0.6 default, 1 mod default)", "height", 1f, "Height");
+    }
+
+    private void Awake()
+    {
+      try
+      {
+        _harmony = Harmony.CreateAndPatchAll(typeof(Main).Assembly, Guid);
+      }
+      catch (Exception e)
+      {
+        Logger.LogError(e);
+      }
+    }
+
+    private void OnDestroy()
+    {
+      try
+      {
+        _harmony?.UnpatchSelf();
+      }
+      catch (Exception e)
+      {
+        Logger.LogError(e);
+      }
+    }
+  }
+}
---
+++ src/Digitalroot.Valheim.SlopeCombatFix/Patch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+
+namespace Digitalroot.Valheim.SlopeCombatFix
+{
+  public static class Patch
+  {
+    [HarmonyPatch(typeof(Attack), nameof(Attack.DoMeleeAttack))]
+    public class PatchCharacterSetHealth
+    {
+      private static void Prefix(ref Attack __instance, ref float ___m_maxYAngle, ref float ___m_attackOffset, ref float ___m_attackHeight)
+      {
+        if (__instance.m_character is not Player || (Player)__instance.m_character != Player.m_localPlayer) return;
+				___m_maxYAngle = 180f;
+        ___m_attackOffset = Main.Instance.Offset.Value;
+        ___m_attackHeight = Main.Instance.Height.Value;
+      }
+    }
+  }
+}
